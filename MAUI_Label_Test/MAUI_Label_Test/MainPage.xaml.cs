4cdd4c7a9983c038d225700a14bc146b04729a44
TripleSM <50290618+TripleSM@users.noreply.github.com>
2023-09-17 14:04:02 +0530

    Add files via upload

---
+++ MAUI_Label_Test/MAUI_Label_Test/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MAUI_Label_Test
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
